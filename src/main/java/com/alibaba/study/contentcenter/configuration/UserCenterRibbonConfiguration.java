20668e42375a7b2fa0d44807ced7f753e0fdab4f
wjy <316057621@qq.com>
2020-01-04 15:17:50 +0800

    使用Java代码的方式配置自定义Ribbon IRule

--- src/main/java/com/alibaba/study/contentcenter/configuration/UserCenterRibbonConfiguration.java
+++ src/main/java/com/alibaba/study/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -1,8 +1,15 @@
 package com.alibaba.study.contentcenter.configuration;
 
+import com.alibaba.study.ribbonconfiguration.RibbonConfiguration;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @Author: wjy
  * @Date: 2020/1/3 22:44
  */
+@Configuration
+@RibbonClient(name = "user-center", configuration = RibbonConfiguration.class)
 public class UserCenterRibbonConfiguration {
+
 }
---
+++ src/main/java/com/alibaba/study/ribbonconfiguration/RibbonConfiguration.java
@@ -0,0 +1,19 @@
+package com.alibaba.study.ribbonconfiguration;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: wjy
+ * @Date: 2020/1/4 14:31
+ */
+@Configuration
+public class RibbonConfiguration {
+    @Bean
+    public IRule ribbonRule(){
+        return new RandomRule();
+    }
+
+}
